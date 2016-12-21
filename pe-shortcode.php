e5da0e0cae824c97935049854db0a90d4bf46564
Emma Edgar <emma.edgar@tunatraffic.com>
2016-12-21 15:47:24 -0600

    adjust formatting

--- pe-shortcode.php
+++ pe-shortcode.php
@@ -11,29 +11,29 @@
 if ( ! shortcode_exists( 'pin-embed' ) ) {
 
 
-function pe_shortcode_atts( $atts ) {
-    $atts = shortcode_atts(
-        array(
-            'url' => '',
-            'size' => 'small',
-            'description' => 'true',
-        ), $atts, 'pin_embed' );
+    function pe_shortcode_atts( $atts ) {
+        $atts = shortcode_atts(
+            array(
+                'url' => '',
+                'size' => 'small',
+                'description' => 'true',
+            ), $atts, 'pin_embed' );
 
 
-    if( $atts['url'] == '' )
-        return;
+        if( $atts['url'] == '' )
+            return;
 
-    wp_enqueue_script('pin-embed-js');
+        wp_enqueue_script('pin-embed-js');
 
 
-    $html = '<a data-pin-do="embedPin" ';
-    if ($atts['size'] == 'medium' || $atts['size'] == 'large') $html .= 'data-pin-width="' . $atts['size'] . '" ';
-    if($atts['description'] == 'false') $html .= 'data-pin-terse="true" ';
-    $html .= 'href="' . $atts['url'] . '"></a>';
+        $html = '<a data-pin-do="embedPin" ';
+        if ($atts['size'] == 'medium' || $atts['size'] == 'large') $html .= 'data-pin-width="' . $atts['size'] . '" ';
+        if($atts['description'] == 'false') $html .= 'data-pin-terse="true" ';
+        $html .= 'href="' . $atts['url'] . '"></a>';
 
-    return $html;
-}
-add_shortcode( 'pin-embed', 'pe_shortcode_atts' );
+        return $html;
+    }
+    add_shortcode( 'pin-embed', 'pe_shortcode_atts' );
 
 
 }
